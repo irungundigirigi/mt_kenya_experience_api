8c27c37e062d2c86680ca36889da4508665b0c0e
irungundigirigi <irungundigirigi@gmail.com>
2024-02-15 15:17:09 +0300

    update server

--- index.js
+++ index.js
@@ -27,17 +27,33 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // POST route to add data to the database
-app.post('/signup', async (req, res) => {
-    const { fullName, username, email, password } = req.body;
+app.post('/share', async (req, res) => {
+    const { email, author, title, body } = req.body;
 
     try {
-        const query = 'INSERT INTO users (full_name, username, email, password) VALUES ($1, $2, $3, $4) RETURNING *';
-        const values = [fullName, username, email, password];
+        const query = 'INSERT INTO posts (email, author, title, body) VALUES ($1, $2, $3, $4) RETURNING *';
+        const values = [email,author, title, body];
         const result = await client.query(query, values);
 
         res.status(201).json(result.rows[0]);
     } catch (error) {
         console.error('Error adding user to database:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+app.get('/posts', async (req, res) => {
+    try {
+        // Query to get all posts
+        const query = 'SELECT * FROM posts';
+
+        // Execute the query
+        const result = await pool.query(query);
+
+        // Send the posts as a JSON response
+        res.status(200).json(result.rows);
+    } catch (error) {
+        // Handle errors
+        console.error('Error fetching posts:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
